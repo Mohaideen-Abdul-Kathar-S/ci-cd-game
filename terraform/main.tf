476feeaef5aa6a91ab110933a0bec8a2e928f48e
Mohaideen Abdul Kathar S <157392970+Mohaideen-Abdul-Kathar-S@users.noreply.github.com>
2026-02-11 10:41:59 +0530

    Change key_name from 'dev.pem' to 'dev'

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "game_server" {
   ami           = "ami-0b6c6ebed2801a5cb" 
   instance_type = "t3.small"
-  key_name      = "dev.pem"
+  key_name      = "dev"
 
   security_groups = [aws_security_group.mygame1.name]
 
